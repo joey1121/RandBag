11c2e3156cd6bc1ba549706ab8e7d8a307598b98
joey1121 <joe_chen1121@hotmail.com>
2016-08-31 10:11:16 -0300

    Add files via upload

---
+++ RandBag/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace RandBag
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.AccessibleDescription = "Click the bar to start.";
+            this.progressBar1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.progressBar1.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.progressBar1.Location = new System.Drawing.Point(12, 114);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.progressBar1.Size = new System.Drawing.Size(126, 16);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 0;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 153);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(49, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "1";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(77, 153);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(49, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "7";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label4.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Maroon;
+            this.label4.Location = new System.Drawing.Point(20, 59);
+            this.label4.Name = "label4";
+            this.label4.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label4.Size = new System.Drawing.Size(104, 51);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "0";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(82, 191);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(42, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "1";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(79, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Numbers";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)), true);
+            this.textBox4.ForeColor = System.Drawing.Color.Red;
+            this.textBox4.Location = new System.Drawing.Point(12, 12);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox4.Size = new System.Drawing.Size(126, 44);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 176);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Time (sec.)";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(20, 192);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(39, 20);
+            this.textBox5.TabIndex = 3;
+            this.textBox5.Text = "3";
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(29, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 20);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(86, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Max";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 137);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Min";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(147, 223);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Lucky Draw";
+            this.TransparencyKey = System.Drawing.Color.Red;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ RandBag/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Media;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandBag
+{
+    public partial class Form1 : Form
+    {
+        private int min=0;
+        private int max=0;
+        private int countdown_t = 0;
+        private int countdown_n = 0;
+        private int draw_size = 0;
+        private int _grid = 20;
+        private string soundsource = null;
+        Timer timerflash = new Timer();
+        Timer timerprogress = new Timer();
+        int progressvalue = 0;
+        Random random = new Random();
+        List<string> drawBag;
+        ToolTip toolBarTip;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timerflash.Tick += timerflash_Tick;
+            timerprogress.Tick += timerprogress_Tick;
+            timerflash.Interval = (57) * (1);
+            int.TryParse(textBox5.Text, out countdown_t);
+            int.TryParse(textBox2.Text, out countdown_n);
+            progressBar1.Visible = false;
+            button1.Visible = true;
+            button1.Select();
+            toolBarTip = new ToolTip();
+            drawBag = new List<string>();
+            toolBarTip.ShowAlways = true;            
+            toolBarTip.SetToolTip(textBox4, "Copy/Paste your Number(s)");
+            toolBarTip.SetToolTip(progressBar1, "Click to Pick a Number");
+            toolBarTip.SetToolTip(button1, "Click to Pick Number(s)");
+        }
+
+        void timerflash_Tick(object sender, EventArgs e)
+        {            
+            int ran;
+
+            ran = random.Next(min, max + 1);
+
+            label4.Text = ran.ToString();
+
+            if (draw_size <= max - min + 1 )
+            while (drawBag.Contains(label4.Text) )
+            {
+                ran = random.Next(min, max + 1);
+                label4.Text = ran.ToString();
+
+            };
+
+
+        }
+
+        void timerprogress_Tick(object sender, EventArgs e)
+        {
+            progressvalue++;
+            if ( (progressvalue%_grid) ==0 )
+            {
+                if (progressvalue > progressBar1.Maximum)
+                {
+                    timerprogress.Enabled = false;
+                }
+                else if (progressBar1.Value > progressvalue)
+                    progressvalue = progressBar1.Value;
+                else
+                {
+                    progressBar1.Value = progressvalue;
+                    if (!picknum())
+                    {
+                        progressvalue = 0;
+                        timerprogress.Enabled = false;
+                    }
+                }
+            }
+            else if (progressvalue <= progressBar1.Maximum)
+            {
+                if (progressBar1.Value > progressvalue)
+                    progressvalue = progressBar1.Value;
+                else progressBar1.Value = progressvalue;
+            }
+        }
+
+        private bool picknum()
+        {
+            if (timerflash.Enabled)
+            {
+                if (countdown_n > 0)
+                {
+                    SoundPlayer soundPlayer = new SoundPlayer(soundsource);
+                    soundPlayer.Play();
+
+                       
+                    if (textBox4.Text.Length == 0)
+                    {
+                        textBox4.Text = label4.Text;
+                    }
+                    else
+                        textBox4.Text += ", " + label4.Text;
+                    textBox4.SelectionStart = textBox4.TextLength;
+                    textBox4.ScrollToCaret();
+                    countdown_n--;
+                    progressBar1.Value = progressBar1.Maximum - countdown_n*_grid;
+                    drawBag.Add(label4.Text);
+                }
+
+                if (countdown_n <= 0)
+                {
+                    timerflash.Enabled = false;
+                    timerflash.Stop();
+                    timerprogress.Enabled = false;
+                    timerprogress.Stop();
+                    progressBar1.Visible = false;
+                    button1.Visible = true;
+                    button1.Select();
+                }
+
+            }
+            else
+            {
+
+                    textBox4.Text = null;
+
+
+                timerflash.Enabled = true;
+                timerprogress.Enabled = true;
+                timerflash.Start();
+                timerprogress.Start();
+            }
+            if (countdown_n <= 0) return false;
+            else return true;
+        }
+
+        private void range_validate() {
+            int org_countdown_n = countdown_n;
+            int org_countdown_t = countdown_t;
+            if (!int.TryParse(textBox1.Text, out min))
+                min = 0;
+
+            if (!int.TryParse(textBox3.Text, out max))
+                max = 0;
+
+            if (!int.TryParse(textBox5.Text, out countdown_t))
+                countdown_t = org_countdown_t;
+
+            if (!int.TryParse(textBox2.Text, out countdown_n))
+                countdown_n = org_countdown_n;
+
+
+            if (min > max)
+            {
+                int temp = min;
+                min = max;
+                max = temp;
+            }
+            textBox1.Text = min.ToString();
+            textBox3.Text = max.ToString();
+            textBox5.Text = countdown_t.ToString();
+            textBox2.Text = countdown_n.ToString();
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = countdown_n*_grid;
+            progressBar1.Value = 0;
+            timerprogress.Interval = countdown_t*1000/countdown_n/_grid;
+            progressvalue = 0;
+            draw_size = countdown_n;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            textBox3.Text = Properties.Settings.Default.Maximum;
+            textBox1.Text = Properties.Settings.Default.Minimum;
+            soundsource = Properties.Settings.Default.SoundSource;
+            textBox5.Text = Properties.Settings.Default.TotalTime;
+            textBox2.Text = Properties.Settings.Default.Number;
+            _grid = Properties.Settings.Default.Grid;
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+            picknum();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.Maximum = textBox3.Text;
+            Properties.Settings.Default.Minimum = textBox1.Text;
+            Properties.Settings.Default.TotalTime = textBox5.Text;
+            Properties.Settings.Default.Number = textBox2.Text;
+            
+            Properties.Settings.Default.Save();
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            range_validate();
+            if (max != min)
+            {
+                drawBag.Clear();
+                button1.Visible = false;
+                progressBar1.Visible = true;
+                progressBar1.Select();
+                picknum();
+            }
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_FormClosing(Object sender, FormClosingEventArgs e)
+        {
+
+        }
+
+    }
+}
